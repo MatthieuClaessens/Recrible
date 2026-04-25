079034a5eca0cd50527a301497f024d7af97ce60
MatthieuClaessens <matthieu-claessens@hotmail.com>
2026-04-25 12:05:40 +0200

    first commit

---
+++ backend/src/main/java/com/recrible/RecribleApplication.java
@@ -0,0 +1,13 @@
+package com.recrible;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecribleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecribleApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/recrible/RecribleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recrible;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecribleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
