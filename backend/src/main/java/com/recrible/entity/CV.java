319b630368a8646d7f3277aa8eba6bbb1cfacb0b
MatthieuClaessens <matthieu-claessens@hotmail.com>
2026-05-13 23:09:26 +0200

    Updated CV and UserAccount entities

--- backend/src/main/java/com/recrible/entity/CV.java
+++ backend/src/main/java/com/recrible/entity/CV.java
@@ -1,11 +1,34 @@
 package com.recrible.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import java.time.LocalDate;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "cv")
 public class CV {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "cv_id")
     private Long CVId;
+
+    @Column(name = "cv_name", nullable = false)
     private String CVName;
+
+    @Column(name = "cv_path", nullable = false)
     private String CVPath;
+
+    @Column(name = "cv_date", nullable = false)
     private LocalDate CVDate;
 
+@ManyToOne(fetch = FetchType.LAZY)
+@JoinColumn(name = "user_account_id", nullable = false)
+    private UserAccount userAccount;
 }
--- backend/src/main/java/com/recrible/entity/UserAccount.java
+++ backend/src/main/java/com/recrible/entity/UserAccount.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Entity
 @Table(name = "user_account")
 @Getter
@@ -12,20 +14,24 @@ public class UserAccount {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "useraccount_id")
     private Long userAccountId;
 
-    @Column(nullable = false, unique = true, length = 30)
+    @Column(name = "account_name", nullable = false, unique = true, length = 30)
     private String accountName;
 
-    @Column(nullable = false, unique = true, length = 50)
+    @Column(name = "email", nullable = false, unique = true, length = 50)
     private String email;
 
-    @Column(nullable = false, unique = true, length = 255)
+    @Column(name = "password", nullable = false, unique = true, length = 255)
     private String password;
 
-    @Column(length = 100)
+    @Column(name = "address", length = 100)
     private String address;
 
-    @Column(length = 20)
+    @Column(name = "phone_number", length = 20, unique = true)
     private String phoneNumber;
+
+    @OneToMany(mappedBy = "userAccount", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<CV> cvs;
 }
