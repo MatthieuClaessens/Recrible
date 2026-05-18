3494a969beff44c2b6c77d78fded71efe6216ee0
MatthieuClaessens <matthieu-claessens@hotmail.com>
2026-05-18 21:24:09 +0200

    Added JobApplicationRepository file

---
+++ backend/src/main/java/com/recrible/repository/JobApplicationRepository.java
@@ -0,0 +1,9 @@
+package com.recrible.repository;
+
+import com.recrible.entity.JobApplication;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface JobApplicationRepository extends JpaRepository<JobApplication, Long> {
+}
