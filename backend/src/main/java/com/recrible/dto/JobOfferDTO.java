04aa25f7056da792fd7b9b31fa19e58abcaf8b96
MatthieuClaessens <matthieu-claessens@hotmail.com>
2026-05-25 16:22:06 +0200

    Added validation messages and updated JobOfferDTO

--- backend/src/main/java/com/recrible/dto/JobOfferDTO.java
+++ backend/src/main/java/com/recrible/dto/JobOfferDTO.java
@@ -1,6 +1,18 @@
 package com.recrible.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 import java.time.LocalDate;
 
-public record JobOfferDTO (Long jobOfferId, String jobOfferTitle, String jobOfferDescription, String jobOfferRequirements, LocalDate jobOfferDate) {
+public record JobOfferDTO (
+        Long jobOfferId,
+
+        @NotBlank(message = "{jobOffer.title.notblank}")
+        String jobOfferTitle,
+
+        @NotBlank(message = "{jobOffer.description.notblank}")
+        String jobOfferDescription,
+        String jobOfferRequirements,
+        LocalDate jobOfferDate) {
 }
