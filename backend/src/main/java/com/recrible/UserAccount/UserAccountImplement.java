9da2772d39dbce0fda024ba90fecc04f8f0676d2
MatthieuClaessens <matthieu-claessens@hotmail.com>
2026-07-04 01:13:45 +0200

    Completed CRUD on UserAccount service

--- backend/src/main/java/com/recrible/UserAccount/UserAccountImplement.java
+++ backend/src/main/java/com/recrible/UserAccount/UserAccountImplement.java
@@ -3,6 +3,7 @@ package com.recrible.UserAccount;
 public interface UserAccountImplement {
 
     UserAccountDTO createUserAccount(UserAccountDTO userAccountDTO);
+    UserAccountDTO readUserAccount(Long userAccountId);
+    UserAccountDTO editUserAccount(UserAccountDTO userAccountDTO);
     void deleteUserAccount(Long userAccountId);
-
 }
--- backend/src/main/java/com/recrible/UserAccount/UserAccountService.java
+++ backend/src/main/java/com/recrible/UserAccount/UserAccountService.java
@@ -13,18 +13,40 @@ public class UserAccountService implements UserAccountImplement {
 
     private final UserAccountRepository userAccountRepository;
 
+    private void ensureUserAccountExists(Long userAccountId) {
+        if (!userAccountRepository.existsById(userAccountId)) {
+            throw new EntityNotFoundException("User account with ID" + userAccountId + " not found");
+        }
+    }
+
     @Override
     public UserAccountDTO createUserAccount(UserAccountDTO userAccountDTO) {
         UserAccount userAccount = userAccountMapper.fromDTO(userAccountDTO);
-        UserAccount savedUserAccount = userAccountRepository.save(userAccount);
-        return userAccountMapper.toDTO(savedUserAccount);
+        return saveAndReturn(userAccount);
+    }
+
+    @Override
+    public UserAccountDTO readUserAccount(Long id) {
+        return userAccountRepository.findById(id)
+                .map(userAccountMapper::toDTO)
+                .orElseThrow(() -> new EntityNotFoundException("User account with ID " + id + " not found"));    }
+
+    @Override
+    public UserAccountDTO editUserAccount(UserAccountDTO userAccountDTO) {
+        UserAccount userAccount = userAccountMapper.fromDTO(userAccountDTO);
+        ensureUserAccountExists(userAccountDTO.userAccountId());
+        return saveAndReturn(userAccount);
     }
 
     @Override
     public void deleteUserAccount(Long userAccountId) {
-        if (!userAccountRepository.existsById(userAccountId)) {
-            throw new EntityNotFoundException("User account with ID" + userAccountId + " not found");
-        }
+        ensureUserAccountExists(userAccountId);
         userAccountRepository.deleteById(userAccountId);
+    }
+
+
+    private UserAccountDTO saveAndReturn(UserAccount userAccount) {
+        UserAccount savedUserAccount = userAccountRepository.save(userAccount);
+        return userAccountMapper.toDTO(savedUserAccount);
     }
 }
