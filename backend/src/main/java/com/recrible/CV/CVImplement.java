4a3893700abe4b6bb9b249facc88ecbe2d768d6d
MatthieuClaessens <matthieu-claessens@hotmail.com>
2026-07-21 15:08:52 +0200

    Implemented creation, read and edit methods for CVService

--- backend/src/main/java/com/recrible/CV/CVImplement.java
+++ backend/src/main/java/com/recrible/CV/CVImplement.java
@@ -2,6 +2,8 @@ package com.recrible.CV;
 
 public interface CVImplement {
 
-    CVDTO createCV(CV cv);
+    CVDTO createCV(CVDTO cvdto);
+    CVDTO readCV(Long cvId);
+    CVDTO editCV(CVDTO cvDTO);
     void deleteCV(Long cvId);
 }
--- backend/src/main/java/com/recrible/CV/CVService.java
+++ backend/src/main/java/com/recrible/CV/CVService.java
@@ -3,6 +3,8 @@ package com.recrible.CV;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import jakarta.persistence.EntityNotFoundException;
+
 
 @Service
 @RequiredArgsConstructor
@@ -12,9 +14,31 @@ public class CVService implements CVImplement {
 
     private final CVRepository cvRepository;
 
-    @Override
-    public CVDTO createCV (CV cv) {
+
+    private void ensureCVExists(Long cvId) {
+        if (!cvRepository.existsById(cvId)) {
+            throw new EntityNotFoundException("CV with ID " + cvId + " not found");
+        }
+    }
+
+    private CVDTO saveAndReturn(CV cv) {
         return cvMapper.toDTO(cvRepository.save(cv));
+    }
+
+    @Override
+    public CVDTO createCV(CVDTO cvDTO) {
+        return saveAndReturn(cvMapper.fromDTO(cvDTO));
+    }
+
+    @Override
+    public CVDTO readCV(Long cvId) {
+        return cvMapper.toDTO(cvRepository.findById(cvId).orElseThrow(() -> new EntityNotFoundException("CV with ID " + cvId + " not found")));
+    }
+
+    @Override
+    public CVDTO editCV(CVDTO cvDTO) {
+        ensureCVExists(cvDTO.cvId());
+        return saveAndReturn(cvMapper.fromDTO(cvDTO));
     }
 
     @Override
