0d9de62d51d31ae8b4e9f1192c53047c47e4eddd
MatthieuClaessens <matthieu-claessens@hotmail.com>
2026-08-17 01:43:17 +0200

    Updated JobOffers component by implementing card display

--- frontend/app/components/JobOffers.tsx
+++ frontend/app/components/JobOffers.tsx
@@ -1,10 +1,47 @@
 import SearchBar from "./SearchBar";
+import { SlidersHorizontal } from "lucide-react";
 
 export default function JobOffers() {
     return (
-        <div className="bg-black w-full min-h-screen md:-mt-15 z-20 absolute">
+        <div className="bg-black w-full min-h-screen md:-mt-15 z-20 absolute text-center">
             <div className="max-w-7xl mx-auto px-10 relative -mt-13 z-30 mb-20">
                 <SearchBar/>
+            </div>
+            <div className="max-w-7xl mx-auto px-10">
+                <div className="grid grid-cols-1 lg:grid-cols-12 gap-8 items-start">
+
+                    <div className="lg:col-span-4 lg:sticky lg:top-8 z-10">
+                        <div className="bg-[#171717] border border-[#262626] rounded-2xl p-6 text-white">
+                            <h3 className="font-semibold text-lg mb-6 flex gap-2">
+                                <SlidersHorizontal /> Filtres
+                            </h3>
+                            <div className="space-y-4 text-gray-400 text-sm">
+                                <p className="uppercase">Type de contrat</p>
+                                <p>CDI</p>
+                            </div>
+                        </div>
+                    </div>
+                    <main className="lg:col-span-8">
+                        <div className="flex justify-between items-center mb-6 text-white">
+                            <p className="text-xl font-medium">12 offres trouvées</p>
+                            <span className="text-gray-400 text-sm">Trier par : Plus récentes</span>
+                        </div>
+                        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                            <div className="bg-[#171717] border border-[#262626] rounded-2xl p-6 text-white">
+                                <p className="text-white">Carte offre 1</p>
+                            </div>
+                            <div className="bg-[#171717] border border-[#262626] rounded-2xl p-6 text-white">
+                                <p className="text-white">Carte offre 2</p>
+                            </div>
+                            <div className="bg-[#171717] border border-[#262626] rounded-2xl p-6 text-white">
+                                <p className="text-white">Carte offre 3</p>
+                            </div>
+                            <div className="bg-[#171717] border border-[#262626] rounded-2xl p-6 text-white">
+                                <p className="text-white">Carte offre 4</p>
+                            </div>
+                        </div>
+                    </main>
+                </div>
             </div>
         </div>
     );
--- frontend/app/components/NavBar.tsx
+++ frontend/app/components/NavBar.tsx
@@ -11,10 +11,10 @@ export default function NavBar() {
             <li className="font-semibold text-xl cursor-pointer">Ressources</li>
         </ul>
         <div className="gap-2 flex flex-row pr-10">
-            <Link href={"/"} className="bg-white px-6 py-2 outline-1 outline-[#9F9F9F] rounded-full">
+            <Link href={"/"} className="bg-white hover:shadow-lg active:scale-95 transition-all duration-200 ease-in-out px-6 py-2 outline-1 outline-[#9F9F9F] rounded-full inline-block">
                 <p>Je recrute</p>
             </Link>
-            <Link href={"/"} className="bg-black text-white px-6 py-2 rounded-full">
+            <Link href={"/"} className="bg-black hover:shadow-lg active:95 transition-all duration:200 ease-in-out text-white px-6 py-2 rounded-full">
                 <p>Se connecter</p>
             </Link>
         </div>
